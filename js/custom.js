635927f06613434775b05443cb561b5c4df63c34
supriyofrontend <supriyo.codearts@gmail.com>
2022-11-09 17:29:53 +0530

    Responsive added

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
 // JavaScript Document
 jQuery('#testimonial-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        800:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
+loop:true,
+margin:10,
+nav:false,
+dots:true,
+responsive:{
+    0:{
+        items:1
+    },
+    600:{
+        items:1
+    },
+    800:{
+        items:2
+    },
+    1000:{
+        items:3
     }
+}
 });
